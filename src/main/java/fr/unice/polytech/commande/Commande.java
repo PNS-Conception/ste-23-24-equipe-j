7b77e4b565e3929a2c4bfbf57f97f758d19371a7
NicolasZanin <nicolas.zanin@etu.univ-cotedazur.fr>
2023-11-04 23:05:05 +0100

    task: création de la super class Commande
    La super classe contient 3 méthodes abstract et l'attribut id et informationLivraison #33

--- src/main/java/fr/unice/polytech/commande/Commande.java
+++ src/main/java/fr/unice/polytech/commande/Commande.java
@@ -1,46 +1,21 @@
 package fr.unice.polytech.commande;
 
 import fr.unice.polytech.livraison.InformationLivraison;
-import fr.unice.polytech.nourriture.MenuPlat;
-import fr.unice.polytech.nourriture.Plat;
-import fr.unice.polytech.restaurant.Restaurant;
-import fr.unice.polytech.utilisateur.CompteUtilisateur;
 
-import java.util.Map;
-import java.util.HashMap;
+import java.util.List;
 import java.util.Objects;
 
 /**
- * Classe d'une commande d'une seul personne
+ * Classe abstraite d'une commande de plusieurs personnes ou d'une seule personne
  * @author Equipe J
  */
-public class Commande {
-    private final int id;
-    private double prixCommande;
-    private final Map<MenuPlat, Integer> menuPlats;
-    private Restaurant restaurant;
-    private EtatCommande etatCommande;
-    private CompteUtilisateur compteUtilisateur;
-    private InformationLivraison informationLivraison;
+public abstract class Commande {
+    protected final int id;
+    protected InformationLivraison informationLivraison;
 
-    /**
-     * Constructeur pour les tests
-     */
-    public Commande(int id){
+    protected Commande(int id) {
         this.id = id;
-        prixCommande = 0;
-        menuPlats = new HashMap<>();
-        etatCommande = EtatCommande.EN_ATTENTE;
         informationLivraison = new InformationLivraison();
-        compteUtilisateur = null;
-    }
-
-    /**
-     * Constructeur par défaut de Commande
-     */
-    public Commande(CompteUtilisateur compteUtilisateur, int id) {
-        this(id);
-        this.compteUtilisateur = compteUtilisateur;
     }
 
     // Accesseurs et setters
@@ -57,40 +32,13 @@ public class Commande {
      * Retourne le prix de la commande
      * @return le prix de la commande
      */
-    public double getPrix() {
-        return prixCommande;
-    }
+    public abstract double getPrix();
 
     /**
      * Retourne l'état de la commande au niveau du client et du restaurant
      * @return l'état de la commande
      */
-    public EtatCommande getEtatCommande() {
-        return etatCommande;
-    }
-
-    /**
-     * Retourne les plats ou menus et les quantités de la commande
-     * @return les plats ou menus et les quantités de la commande
-     */
-    public Map<MenuPlat, Integer> getMenuPlats() {
-        return menuPlats;
-    }
-
-    /**
-     * Retourne le restaurant contenant les plats ou menus de la commande
-     * @return le restaurant contenant les plats ou menus de la commande
-     */
-    public Restaurant getRestaurant() {
-        return restaurant;
-    }
-
-    /**
-     * Retourne le compte utilisateur qui a passé la commande
-     */
-    public CompteUtilisateur getCompteUtilisateur() {
-        return compteUtilisateur;
-    }
+    public abstract EtatCommande getEtatCommande();
 
     /**
      * Retourne les informations de livraison de la commande
@@ -98,15 +46,6 @@ public class Commande {
      */
     public InformationLivraison getInformationLivraison() {
         return informationLivraison;
-    }
-
-
-    /**
-     * Change le statut de la commande quand elle est terminée pour le client ou restaurant
-     * @param etatCommande le nouveau statut de la commande
-     */
-    public void setEtatCommande(EtatCommande etatCommande) {
-        this.etatCommande = etatCommande;
     }
 
     /**
@@ -117,61 +56,31 @@ public class Commande {
         this.informationLivraison = informationLivraison;
     }
 
-    // Méthodes
+    /**
+     * Méthode pour savoir si c'est une commande individuelle ou groupée
+     * @return <code>true</code> si la commande est groupées, sinon <code>false</code>
+     */
+    public abstract boolean estGroupee();
 
     /**
-     * Ajoute un plat ou un menu avec une quantite à la commande
-     * @param menuPlat le plat ou menu à ajouter
-     * @param quantite la quantite du plat ou menu à ajouter
-     * @throws IllegalArgumentException si la quantite est négative ou
-     * si le plat ou menu n'est pas du même restaurant que la commande
+     * Retourne la liste des commandes individuelles d'une commande
+     * @return la liste des commandes individuelles de la commande groupée
      */
-    public void ajoutMenuPlat(MenuPlat menuPlat, int quantite){
-        if (quantite < 0) {
-            throw new IllegalArgumentException("La quantité doit être positive");
-        }
+    public abstract List<CommandeIndividuelle> getCommandes();
 
-        if (restaurant == null)
-            restaurant = menuPlat.getRestaurant();
-        else if (!restaurant.equals(menuPlat.getRestaurant()))
-            throw new IllegalArgumentException("Le plat ou menu n'est pas du même restaurant que la commande");
-
-        if (menuPlats.containsKey(menuPlat)){
-            int nouvelleQuantite = menuPlats.get(menuPlat) + quantite;
-            menuPlats.put(menuPlat, nouvelleQuantite);
-        }
-        else {
-            menuPlats.put(menuPlat, quantite);
-        }
-        prixCommande += menuPlat.getPrix();
-    }
-
-    // A revoir si on ajoute une quantite à supprimer d'un plat
-    public void suppressionPlat(Plat plat){
-        if (menuPlats.containsKey(plat)){
-            int quantite = menuPlats.get(plat) - 1;
-            if (quantite == 0){
-                menuPlats.remove(plat);
-            }
-            else {
-                menuPlats.put(plat, quantite);
-            }
-        }
-    }
 
     // Equals et HashCode
     @Override
     public boolean equals(Object o) {
         if (this == o)
             return true;
-        if (o == null || Commande.class != o.getClass())
+        if (!(o instanceof Commande commande))
             return false;
-        Commande commande = (Commande) o;
         return id == commande.id;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hash(id, estGroupee());
     }
 }
