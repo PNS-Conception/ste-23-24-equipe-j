7086351a2bb6178bb7b6dd98ef652b342120cadb
ChloeVandenbrulle <chloe.vandenbrulle@etu.unice.fr>
2023-11-07 15:53:19 +0100

    US: complétion de US ajout plat à commande

--- src/test/java/fr/unice/polytech/commande/AjouterPlatALaCommande.java
+++ src/test/java/fr/unice/polytech/commande/AjouterPlatALaCommande.java
@@ -1,33 +1,85 @@
 package fr.unice.polytech.commande;
 
+import fr.unice.polytech.nourriture.MenuPlat;
 import fr.unice.polytech.nourriture.Plat;
+import fr.unice.polytech.restaurant.AucunMenuException;
+import fr.unice.polytech.restaurant.Restaurant;
+import fr.unice.polytech.restaurant.RestaurantManager;
+import io.cucumber.java.BeforeAll;
 import io.cucumber.java.fr.*;
+import org.junit.Before;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class AjouterPlatALaCommande {
+    private final RestaurantManager restaurantManager = new RestaurantManager();
+    private Restaurant restaurant;
     Commande commande;
     Plat plat;
 
     @Etantdonnéque("une commande en cours de création avec un montant qui s'élève à {double}€")
     public void createCommande(double prix){
+        List<String> aliments = new ArrayList<>(Arrays.asList("Tagliatelles", "Saumon", "Crème Fraiche"));
+        List<String> alergenes =  new ArrayList<>();
+        Plat newPlat = new Plat("tagliatelles au saumon", 10, aliments, alergenes);
+
+        restaurant = new Restaurant("Italien");
+        restaurant.addMenu(newPlat);
+
         commande = new Commande();
         assertEquals(prix, commande.getPrix(), 0);
     }
 
+    @Quand("l'utilisateur séléctionne le plat {string}")
+    public void selectionPlat(String nomPlat) {
+        try {
+            for (MenuPlat menuPlat : restaurant.getMenus()){
+                if (menuPlat.getNom().equals(nomPlat) && menuPlat.getClass().equals(Plat.class)){
+                    plat = (Plat) menuPlat;
+                    break;
+                }
+            }
+            assertNotNull(plat);
+        }
+        catch (AucunMenuException e){
+            System.err.println(e);
+        }
+
+    }
+
+    @Alors("l'utilisateur récupère la liste des aliments du plat")
+    public void recupereAliments(){
+        List<String> aliments = plat.getAliments();
+        assertNotNull(aliments);
+    }
+
+    @Et("l'utilisateur récupère la liste des alergenes du plat")
+    public void recupereAlergenes(){
+        List<String> alergenes = plat.getAlergenes();
+        assertNotNull(alergenes);
+    }
+
     @Quand("l'utilisateur ajoute {int} quantité de plat de {string} à {double}€")
-    public void ajoutPlatDansCommande(int quantite, String nomPlat, double prix){
-        List<String> aliments = new ArrayList<>(Arrays.asList("Tagliatelles", "Saumon", "Crème Fraiche"));
-        List<String> alergene =  new ArrayList<>();
-        plat = new Plat(nomPlat, prix, aliments, alergene);
+    public void ajoutPlatDansCommande(int quantite, String nomPlat, double prix) {
+        try {
+            for (MenuPlat menuPlat : restaurant.getMenus()){
+                if (menuPlat.getNom().equals(nomPlat)){
+                    plat = (Plat) menuPlat;
+                    break;
+                }
+            }
 
-        commande.ajoutMenuPlat(plat, quantite);
-
-        int nombrePlatAjouter = commande.getMenuPlats().get(plat);
-        assertEquals(quantite, nombrePlatAjouter);
+            commande.ajoutMenuPlat(plat, quantite);
+            int nombrePlatAjouter = commande.getMenuPlats().get(plat);
+            assertEquals(quantite, nombrePlatAjouter);
+        }
+        catch (AucunMenuException e){
+            System.err.println(e);
+        }
     }
 
     @Alors("{int} quantité de ce plat sera dans ma commande")
