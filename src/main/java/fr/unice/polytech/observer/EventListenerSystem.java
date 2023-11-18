0e23fd8a7e7666f5e4852ae086959c905527b115
NicolasZanin <nicolas.zanin@etu.univ-cotedazur.fr>
2023-11-18 19:34:51 +0100

    fix: oublie de commentaire
    Ajout des commentaire sur l'interface EventListenerSystem

--- src/main/java/fr/unice/polytech/observer/EventListenerSystem.java
+++ src/main/java/fr/unice/polytech/observer/EventListenerSystem.java
@@ -2,6 +2,14 @@ package fr.unice.polytech.observer;
 
 import fr.unice.polytech.commande.Commande;
 
+/**
+ * Interface permettant que certains éléments du système soit notifié d'un changement de status de commande
+ * @author Equipe J
+ */
 public interface EventListenerSystem {
+    /**
+     * Notifie un élément du système
+     * @param commande la commande qui a changé de status
+     */
     public void notify(Commande commande);
 }
