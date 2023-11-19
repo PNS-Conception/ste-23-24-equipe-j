b642d7d9819285fdaf3764329b4c8799f1a4e796
NicolasZanin <nicolas.zanin@etu.univ-cotedazur.fr>
2023-11-20 00:50:18 +0100

    task: création des interfaces payable et livrable
    Création des interfaces payable et livrables qui ont des méthodes pour récupérer le prix ou des méthode pour récupérer le status de livraison #67

---
+++ src/main/java/fr/unice/polytech/commande/interfacecommande/IPayable.java
@@ -0,0 +1,13 @@
+package fr.unice.polytech.commande.interfacecommande;
+
+/**
+ * Interface pour les commande payables
+ * @author Equipe J
+ */
+public interface IPayable {
+    /**
+     * Retourne le prix de la commande
+     * @return le prix de la commande
+     */
+    double getPrix();
+}
