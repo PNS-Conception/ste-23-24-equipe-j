f612095fbb57fd21384f01c45ec48dd31ab4736e
NicolasZanin <nicolas.zanin@etu.univ-cotedazur.fr>
2023-11-20 19:51:21 +0100

    task: ajout de l'interface ICommandeAjoutable
    cette commande implémentera donc ICommande (modifié) et l'interface IPayable #86

--- src/main/java/fr/unice/polytech/commande/interfacecommande/ICommande.java
+++ src/main/java/fr/unice/polytech/commande/interfacecommande/ICommande.java
@@ -1,12 +1,13 @@
-package fr.unice.polytech.commande;
+package fr.unice.polytech.commande.interfacecommande;
 
+import fr.unice.polytech.commande.EtatCommande;
 import fr.unice.polytech.utilisateur.CompteUtilisateur;
 
 /**
  * Classe abstraite des commandes
  * @author Equipe J
  */
-public interface Commande {
+public interface ICommande {
 
     // Getters et setters
 
@@ -14,17 +15,25 @@ public interface Commande {
      * Retourne le créateur de la commande
      * @return le créateur de la commande
      */
-    public CompteUtilisateur getCreateur();
+    CompteUtilisateur getCreateur();
 
     /**
      * Retourne l'état de suivi d'une commande
      * @return l'état de la commande
      */
-    public EtatCommande getEtatCommande() ;
+    EtatCommande getEtatCommande() ;
 
     /**
      * Modifie l'état de suivi d'une commande
      * @param etatCommande le nouvel état de la commande
      */
-    public abstract void setEtatCommande(EtatCommande etatCommande);
+    void setEtatCommande(EtatCommande etatCommande);
+
+    // Méthode
+
+    /**
+     * Retourne si la commande est une commande simple
+     * @return <code>true</code> si la commande est une commande simple, <code>false</code> sinon
+     */
+    boolean estCommandeSimple();
 }
