c4c83d2c41c04ecc895cfb2c4ca1906547d89001
NicolasZanin <nicolas.zanin@etu.univ-cotedazur.fr>
2023-11-20 20:10:54 +0100

    task: ajout de nouvelles méthodes SystemeCommande
    Ce système de commandes aura donc la possibilité de supprimer des commandes, ajouter des commmandes ajoutables et des commandes groupe #87

--- src/main/java/fr/unice/polytech/commande/SystemeCommande.java
+++ src/main/java/fr/unice/polytech/commande/SystemeCommande.java
@@ -1,10 +1,16 @@
 package fr.unice.polytech.commande;
 
 import fr.unice.polytech.builder.*;
+import fr.unice.polytech.commande.interfacecommande.ICommande;
+import fr.unice.polytech.commande.interfacecommande.ICommandeAjoutable;
 import fr.unice.polytech.restaurant.Restaurant;
 import fr.unice.polytech.utilisateur.CompteUtilisateur;
 import java.util.*;
 
+/**
+ * Système de commande qui possède toutes les commandes de la création à la livraison
+ * @author Equipe J
+ */
 public class SystemeCommande {
     private long idCommande;
     private final Map<Integer, CommandeAvecID> commandes;
@@ -35,14 +41,41 @@ public class SystemeCommande {
         return commande;
     }
 
-    // Méthodes
+    /**
+     * Créer une commande simple pour une commande groupe
+     * @param utilisateur le créateur de la commande
+     * @param typeCommandeAjoutable le type de la commande (simple)C
+     * @param idCommandeGroupe l'id de la commande groupe
+     * @return la commande ajoutée dans la commande groupe
+     */
+    public ICommandeAjoutable creerCommandeAjoutable(CompteUtilisateur utilisateur,
+                                                     TypeCommandeAjoutable typeCommandeAjoutable,
+                                                     int idCommandeGroupe) {
+        CommandeAvecID commande = commandes.get(idCommandeGroupe);
+
+
+        if (commande == null || commande.estCommandeSimple())
+            return null;
+        BuilderCommandeSimpleMultiplePourGroupe builder = new BuilderCommandeSimpleMultiplePourGroupe(idCommande++,
+                utilisateur, (ACommandeGroupe) commande);
+        return builder.setTypeCommandeSimple(typeCommandeAjoutable).build();
+    }
+
+    /**
+     * Supprime une commande du système
+     * @param idCommande l'id de la commande
+     */
+    public void supprimerCommande(int idCommande) {
+        commandes.remove(idCommande);
+    }
+
 
     /**
      * Récupère la commande par un id
      * @param idCommande l'id de la commande
      * @return la commande si l'id existe sinon <code>null</code>
      */
-    public Commande getCommandeParId(int idCommande) {
+    public ICommande getCommandeParId(int idCommande) {
         return commandes.get(idCommande);
     }
 
@@ -50,16 +83,18 @@ public class SystemeCommande {
      * Récupère la liste des commandes en préparation du restaurant
      * @return la liste des commandes en préparation du restaurant
      */
-    public List<Commande> getCommandeEnPreparationRestaurant(Restaurant restaurant) {
-        List<Commande> commandesRestaurant = new ArrayList<>();
+    public List<ICommande> getCommandeEnPreparationRestaurant(Restaurant restaurant) {
+        List<ICommande> commandesRestaurant = new ArrayList<>();
 
         for (CommandeAvecID commande : commandes.values()) {
-            if (commande.getEtatCommande().equals(EtatCommande.EN_PREPARATION)) {
+            if ( commande.estCommandeSimple() && commande.getEtatCommande().equals(EtatCommande.EN_PREPARATION)) {
                 Optional<Restaurant> restaurantCommande = ((CommandeSimpleAvecID) commande).getRestaurant();
 
                 if (restaurantCommande.isPresent() && restaurantCommande.get().equals(restaurant))
                     commandesRestaurant.add(commande);
             }
+            else if(!commande.estCommandeSimple())
+                commandesRestaurant.addAll(((ACommandeGroupe) commande).getCommandesEnPreparationRestaurant(restaurant));
         }
 
         return commandesRestaurant;
@@ -70,8 +105,8 @@ public class SystemeCommande {
      * @param restaurant le restaurant où on veut récupérer ses commandes
      * @return la liste des commandes du restaurant
      */
-    public List<Commande> getCommandesRestaurant(Restaurant restaurant) {
-        List<Commande> commandesRestaurant = new ArrayList<>();
+    public List<ICommande> getCommandesRestaurant(Restaurant restaurant) {
+        List<ICommande> commandesRestaurant = new ArrayList<>();
 
         for (CommandeAvecID commande : commandes.values()) {
             Optional<Restaurant> restaurantCommande = ((CommandeSimpleAvecID) commande).getRestaurant();
