b32eb5e264bd3f00fee2b8d7f5c84c0e4ad133da
guillaumeArrigoni <guillaume.arr@gmail.com>
2023-11-21 13:09:34 +0100

    Feature : Update and finish user statistique #42

--- src/test/java/fr/unice/polytech/restaurant/StatistiqueUtilisateur.java
+++ src/test/java/fr/unice/polytech/restaurant/StatistiqueUtilisateur.java
@@ -1,25 +1,96 @@
 package fr.unice.polytech.restaurant;
 
+import fr.unice.polytech.commande.Commande;
+import fr.unice.polytech.commande.CommandeManager;
+import fr.unice.polytech.nourriture.Menu;
+import fr.unice.polytech.nourriture.MenuPlat;
+import fr.unice.polytech.utilisateur.CompteUtilisateur;
 import io.cucumber.java.fr.Alors;
 import io.cucumber.java.fr.Etantdonnéque;
 import io.cucumber.java.fr.Etque;
 import io.cucumber.java.fr.Quand;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import static org.junit.Assert.assertEquals;
+
 public class StatistiqueUtilisateur {
+
+    private CompteUtilisateur compteUtilisateur;
+    private final RestaurantManager restaurantManager = new RestaurantManager();
+    private final CommandeManager commandeManager = new CommandeManager();
+
+    @Etantdonnéque("L'utilisateur peut accéder aux restaurants suivant :")
+    public void lUtilisateurPeutAccéderAuxRestaurantsSuivant(List<String> restaurants) {
+        for (String restaurant : restaurants) {
+            restaurantManager.addRestaurant(new Restaurant(restaurant));
+        }
+    }
+
+    @Etque("le restaurant {string} propose les menus suivant :")
+    public void leRestaurantProposeLesMenusSuivant(String nomRestaurant, Map<String, Double> menus) {
+
+        Restaurant restaurant = restaurantManager.getRestaurantParNom(nomRestaurant);
+        for (Map.Entry<String, Double> menu : menus.entrySet()) {
+            restaurant.addMenu(new Menu(menu.getKey(), menu.getValue()));
+        }
+
+        try {
+            assertEquals(menus.size(), restaurant.getMenus().size());
+        } catch (AucunMenuException e) {
+            assert false : "Le restaurant ne doit pas avoir aucun menu";
+        }
+    }
 
     @Etantdonnéque("l'utilisateur {string} {string} est connecté")
     public void lUtilisateurEstLoggé(String prenom, String nom) {
+        compteUtilisateur = new CompteUtilisateur(nom, prenom);
+        assertEquals(prenom, compteUtilisateur.getPrenom());
+        assertEquals(nom, compteUtilisateur.getNom());
     }
 
     @Etque("{string} {string} n'a jamais effectué de commande")
     public void nAJamaisEffectuéDeCommande(String prenom, String name) {
+        if (Objects.equals(compteUtilisateur.getNom(), name) && Objects.equals(compteUtilisateur.getPrenom(), prenom)) {
+            assertEquals(0, compteUtilisateur.getHistoriqueCommandes().size());
+        } else {
+            assert false : "L'utilisateur n'est pas le bon";
+        }
     }
 
     @Quand("{string} {string} accède à sa page de statistique, il obtient une valeur {int}.")
     public void accèdeÀSaPageDeStatistiqueIlObtientUneValeur(String prenom, String name, int tailleStatistique) {
+        if (Objects.equals(compteUtilisateur.getNom(), name) && Objects.equals(compteUtilisateur.getPrenom(), prenom)) {
+            try {
+                assertEquals(tailleStatistique, (int) compteUtilisateur.getStatUser().get(compteUtilisateur));
+            } catch (PasswordException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            assert false : "L'utilisateur n'est pas le bon";
+        }
     }
 
-    @Alors("il effectue une commande dans ce restaurant {string}.")
-    public void ilEffectueUneCommandeDansCeRestaurant(String nomRestaurant) {
+    @Alors("{string} {string} effectue une commande dans le restaurant {string}.")
+    public void ilEffectueUneCommandeDansCeRestaurant(String prenom, String nom, String nomRestaurant) throws AucunMenuException, PasswordException, TokenException {
+        Restaurant restaurant = restaurantManager.getRestaurantParNom(nomRestaurant);
+        Commande commande = commandeManager.creerCommande(compteUtilisateur);
+
+        List<MenuPlat> listMenus = restaurant.getMenus();
+        if (listMenus.size()!=0) {
+            try {
+                commande.ajoutMenuPlat(listMenus.get(0),1);
+                assertEquals(1, commande.getMenuPlats().size());
+            } catch (CapaciteDepasseException e) {
+                assert false : "Impossible d'ajouter une commande";
+            }
+        } else {
+            assert false : "Le restaurant n'a pas de menu";
+        }
+        commandeManager.payerCommande(commande, this.compteUtilisateur.createToken(CompteUtilisateur.DEFAULT_PASSWORD));
+
     }
 }
--- src/test/resources/features/commande/Statistique_utilisateur.feature
+++ src/test/resources/features/commande/Statistique_utilisateur.feature
@@ -5,9 +5,19 @@
 #language:fr
 Fonctionnalité: Accès aux statistiques des utilisateurs
 
+  Contexte:
+    Etant donné que L'utilisateur peut accéder aux restaurants suivant :
+      | Chinois |
+      | RoastBeef |
+      | AfriqueDuSud |
+      | SophiaRestaurantTech |
+    Et que le restaurant "AfriqueDuSud" propose les menus suivant :
+      | Tricheur | 5,00 |
+      | Voleur | 3,00 |
+
   Scénario: Normal
     Etant donné que l'utilisateur "Antoine" "Dupont" est connecté
     Et que "Antoine" "Dupont" n'a jamais effectué de commande
     Quand "Antoine" "Dupont" accède à sa page de statistique, il obtient une valeur 0.
-    Alors il effectue une commande dans ce restaurant "ADS".
+    Alors "Antoine" "Dupont" effectue une commande dans le restaurant "AfriqueDuSud".
     Quand "Antoine" "Dupont" accède à sa page de statistique, il obtient une valeur 1.
