1213875073a813c1203e9b6e3fa11f8ffa5833d1
guillaumeArrigoni <guillaume.arr@gmail.com>
2023-11-24 02:16:59 +0100

    Feature : Add new statistic to match requirements #104

--- src/main/java/fr/unice/polytech/traçabilite/Statistique.java
+++ src/main/java/fr/unice/polytech/traçabilite/Statistique.java
@@ -5,14 +5,19 @@ import fr.unice.polytech.commande.CommandeSimpleAvecID;
 import fr.unice.polytech.exceptions.PasswordException;
 import fr.unice.polytech.restaurant.Restaurant;
 import fr.unice.polytech.utilisateur.CompteUtilisateur;
+import fr.unice.polytech.utils.Position;
+import fr.unice.polytech.utils.temps.HoraireDate;
 
 import java.util.HashMap;
 
 public class Statistique {
 
     private final String UserPassword = "0000";
+    private final String RestaurantPassword = "1111";
     HashMap<CompteUtilisateur,Integer> nbCommandeUtilisateur;
     HashMap<Restaurant,Integer> nbCommandeRestaurant;
+    HashMap<HoraireDate,Integer> nbCommandeHoraire;
+    HashMap<Position,Integer> nbCommandePosition;
 
 
     public Statistique() {
@@ -22,18 +27,35 @@ public class Statistique {
 
     public boolean updateUserStat(CommandeAvecID commande, String password) {
         if (password.equals(UserPassword)) {
+
             Restaurant restaurant = ((CommandeSimpleAvecID) commande).getRestaurant().orElse(null);
             CompteUtilisateur compteUtilisateur = commande.getCreateur();
+            Position position = commande.getInformationLivraison().getLieuxLivraison();
+
             if (nbCommandeRestaurant.containsKey(restaurant)) {
                 nbCommandeRestaurant.put(restaurant, nbCommandeRestaurant.get(restaurant) + 1);
             } else {
                 nbCommandeRestaurant.put(restaurant, 1);
             }
+
             if (nbCommandeUtilisateur.containsKey(compteUtilisateur)) {
                 nbCommandeUtilisateur.put(compteUtilisateur, nbCommandeUtilisateur.get(compteUtilisateur) + 1);
             } else {
                 nbCommandeUtilisateur.put(compteUtilisateur, 1);
             }
+
+            if (nbCommandePosition.containsKey(position)) {
+                nbCommandePosition.put(position, nbCommandePosition.get(position) + 1);
+            } else {
+                nbCommandePosition.put(position, 1);
+            }
+
+            if (nbCommandeHoraire.containsKey(commande.getInformationLivraison().getHoraireDate())) {
+                nbCommandeHoraire.put(commande.getInformationLivraison().getHoraireDate(), nbCommandeHoraire.get(commande.getInformationLivraison().getHoraireDate()) + 1);
+            } else {
+                nbCommandeHoraire.put(commande.getInformationLivraison().getHoraireDate(), 1);
+            }
+
             return true;
         }
         return false;
@@ -51,6 +73,15 @@ public class Statistique {
             return nbCommandeRestaurant;
         }
         throw new PasswordException();
+    }
+
+    public HashMap<HoraireDate,Integer> getHoraireStat() {
+        return nbCommandeHoraire;
+    }
+
+    public HashMap<Position,Integer> getPositionStat() {
+        return nbCommandePosition;
+
     }
 
 
