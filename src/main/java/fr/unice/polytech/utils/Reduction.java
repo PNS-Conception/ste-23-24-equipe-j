7a00940e373c7c621b263d45b9cf5ef66470f298
Nicolas Zanin <nicolas.zanin@etu.univ-cotedazur.fr>
2023-11-26 16:32:00 +0100

    task: ajout JavaDoc
    Ajout javadoc classe Reduction #105

--- src/main/java/fr/unice/polytech/utils/Reduction.java
+++ src/main/java/fr/unice/polytech/utils/Reduction.java
@@ -4,14 +4,27 @@ import fr.unice.polytech.nourriture.MenuPlat;
 
 import java.util.Map;
 
+/**
+ * Classe traitant la réduction d'une commande
+ * @author Equipe J
+ */
 public class Reduction {
     public static final double R = 0.05;
     public static final double N = 10;
 
+    /**
+     * Méthode privé
+     * @throws IllegalArgumentException class static
+     */
     private Reduction() {
         throw new IllegalArgumentException("Utility class");
     }
 
+    /**
+     * Méthode pour avoir les réductions d'une commande
+     * @param produitsCommandes les menus et/ou plats des commandes
+     * @return le prix réduit si on peut obtenir la réduction
+     */
     protected static double getReduction(Map<MenuPlat, Integer> produitsCommandes) {
         double sum = 0;
         int nombreProduits = 0;
